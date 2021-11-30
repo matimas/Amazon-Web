a0d3dc02f39333772973669b1017ac8230aa42ac
mati misganow <45734976+matimas@users.noreply.github.com>
2021-11-30 10:02:17 +0200

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,11 @@ function increm() {
 	const count = document.querySelector('.counter');
 	count.textContent++;
 }
+const hoverImg = document.getElementById('item1');
+hoverImg.onmouseover = picMovingIn;
+const firstImage = document.getElementById('image1');
+
+function picMovingIn() {
+	console.log(firstImage);
+	firstImage.style.display = 'none';
+}
